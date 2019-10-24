6f6b78337553d38986904405958126732c0be612
Lucas Mendes <mrlukasmendes@gmail.com>
2019-10-24 19:18:08 -0300

    inicio ConfirmaCadastroScreen

---
+++ src/views/ConfirmaCadastroScreen.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {View, Text, Button} from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
+
+const ConfirmaCadastro = () => (
+    <LinearGradient
+        colors={ ['#3CB371', '#2E8B57', '#008000', '#228B22'] }
+        style={ estilos.container }    
+    >
+        <View>
+            <Text>AAA</Text>
+        </View>
+    </LinearGradient>
+
+);
