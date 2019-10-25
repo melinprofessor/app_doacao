fff10c4962449fe38ecf74a93e44c0c04f50caf9
Lucas <mrlukasmendes@gmail.com>
2019-10-24 22:35:19 -0300

    continuando ConfirmaCadastroScreen

--- src/routes/AppNavigator.js
+++ src/routes/AppNavigator.js
@@ -2,9 +2,11 @@ import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createDrawerNavigator } from 'react-navigation-drawer';
 import LoginScreen from '../views/LoginScreen';
+import ConfirmaCadastroScreen from '../views/ConfirmaCadastroScreen';
 
 const createStack = createStackNavigator({
   login: LoginScreen,
+  confirmaCadastro: ConfirmaCadastroScreen,
 });
 
 export default createAppContainer(createStack);
--- src/views/ConfirmaCadastroScreen.js
+++ src/views/ConfirmaCadastroScreen.js
@@ -1,56 +1,68 @@
+
 import React from 'react';
-import {View, Text, Button, Platform} from 'react-native';
+import { View, Text, Platform } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 import ButtonAndroidComponent from '../components/ButtonAndroidComponent';
 import StyleSheet, { estilos } from '../styles/styles';
 import ButtonIOSComponent from '../components/ButtonIOSComponent';
 
 export function navigationOptions({ navigate }) {
-    return {
-      title: 'ConfirmaCadastro',
-      header: null,
-    };
-  }
-
-  const ButtonComponent = Platform.select({
-    ios: () => ButtonIOSComponent,
-    android: () => ButtonAndroidComponent,
-  })();
+  return {
+    title: 'ConfirmaCadastro',
+    header: null,
+  };
+}
+const ButtonComponent = Platform.select({
+  ios: () => ButtonIOSComponent,
+  android: () => ButtonAndroidComponent,
+})();
 
 const ConfirmaCadastro = () => (
-    <LinearGradient
-        colors={ ['#3CB371', '#2E8B57', '#008000', '#228B22'] }
-        style={ estilos.container }    
+  <LinearGradient
+    colors={ ['#3CB371', '#2E8B57', '#008000', '#228B22'] }
+    style={ estilos.container }
     >
-        <View>
-            <View>
-                <View>
-                    <Text>Usuário</Text>
-                    <Text>Cadastrado</Text>
-                    <Text>com Sucesso!</Text>
-                </View>                
-            </View>
-            <View>
-                <View>
-                    <Text>TESTE</Text>
-                </View>
-                <ButtonComponent title='FAZER LOGIN'/>
-            </View>
+    <View>
+      <View style={ estilo.container }>
+        <View style={ estilo.viewTexto }>
+          <Text>Usuário</Text>
+          <Text>Cadastrado</Text>
+          <Text>com Sucesso!</Text>
         </View>
-    </LinearGradient>
+      </View>
+      <View>
+        <View>
+          <Text>TESTE</Text>
+        </View>
+        <ButtonComponent title='FAZER LOGIN' />
+      </View>
+      <View>
+        <Text>
+                    Confirme o e-mail recebido.
+        </Text>
+      </View>
+    </View>
+  </LinearGradient>
 
 );
 
 const ConfirmaCadastroScreen = {
-    screen: CpnfirmaCadastro,
-    navigationOptions,
-  };
+  screen: ConfirmaCadastro,
+  navigationOptions,
+};
 
 const estilo = StyleSheet.create({
-    container: {
-        alignItems: 'center',
-        justifyContent: 'center',
-      },
-})
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  viewTexto: {
+    borderLeftWidth: 10,
+    borderLeftColor: '#fff',
+    borderTopLeftRadius: 10,
+    borderBottomLeftRadius: 10,
+    padding: 15,
+  },
+});
 
 export default ConfirmaCadastroScreen;
