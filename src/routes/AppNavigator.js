aab9550d7ca8cebfe9a6c1ae04e587ca5a6e620b
Lucas <mrlukasmendes@gmail.com>
2019-10-28 20:45:26 -0300

    Tela ConfirmaEmail Finalizada

--- src/routes/AppNavigator.js
+++ src/routes/AppNavigator.js
@@ -2,8 +2,10 @@ import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createDrawerNavigator } from 'react-navigation-drawer';
 import LoginScreen from '../views/LoginScreen';
+import ConfirmaEmailScreen from '../views/ConfirmaEmailScreen';
 
 const createStack = createStackNavigator({
+  confirmaEmail: ConfirmaEmailScreen,
   login: LoginScreen,
 });
 
