b4cb03a7e39ebf91de09a850eccfe53ffeeda266
BRENO MATEUS FERNANDES <breno.mfernandes@senacsp.edu.br>
2019-10-30 22:26:47 -0300

    finalizando

--- src/routes/AppNavigator.js
+++ src/routes/AppNavigator.js
@@ -2,8 +2,10 @@ import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createDrawerNavigator } from 'react-navigation-drawer';
 import LoginScreen from '../views/LoginScreen';
+import SolicitarScreen from '../views/SolicitarScreen';
 
 const createStack = createStackNavigator({
+  solicitar: SolicitarScreen,  
   login: LoginScreen,
 });
 
