3eca01d9a2bbc18039d4f2635bd6d546e8dd2ceb
Lucas <mrlukasmendes@gmail.com>
2019-10-30 22:26:29 -0300

    tela doarScreen

--- src/routes/AppNavigator.js
+++ src/routes/AppNavigator.js
@@ -2,8 +2,10 @@ import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createDrawerNavigator } from 'react-navigation-drawer';
 import LoginScreen from '../views/LoginScreen';
+import DoarScreen from '../views/DoarScreen';
 
 const createStack = createStackNavigator({
+  doar: DoarScreen,
   login: LoginScreen,
 });
 
