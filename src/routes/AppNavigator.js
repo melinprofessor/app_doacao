bb12a32358e78763b539c846b40dfa56acceb253
marciobebedouro@hotmail.com <MY_NAME@example.com>
2019-11-01 22:15:50 -0300

    PedidosScreen

--- src/routes/AppNavigator.js
+++ src/routes/AppNavigator.js
@@ -2,9 +2,12 @@ import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createDrawerNavigator } from 'react-navigation-drawer';
 import LoginScreen from '../views/LoginScreen';
+import PedidosScreen from '../views/PedidosScreen';
 
 const createStack = createStackNavigator({
+  pedidos: PedidosScreen,
   login: LoginScreen,
+ 
 });
 
 export default createAppContainer(createStack);
