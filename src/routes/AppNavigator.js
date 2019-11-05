ca0001c5d46cf1fd77b7bdb532d134655a977230
BRENO MATEUS FERNANDES <breno.mfernandes@senacsp.edu.br>
2019-11-04 21:16:08 -0300

    finalizando o infoScreen

--- src/routes/AppNavigator.js
+++ src/routes/AppNavigator.js
@@ -2,8 +2,10 @@ import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createDrawerNavigator } from 'react-navigation-drawer';
 import LoginScreen from '../views/LoginScreen';
+import InfoScreen from '../views/InfoScreen';
 
 const createStack = createStackNavigator({
+  Info: InfoScreen,
   login: LoginScreen,
 });
 
