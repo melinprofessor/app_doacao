d9e2caf9fe8ee40919fb01060d01fc806bb51f19
Lucas <mrlukasmendes@gmail.com>
2019-11-04 22:32:32 -0300

    telaDoar

--- src/views/DoarScreen.js
+++ src/views/DoarScreen.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import { View, TextInput } from 'react-native';
+import React, {useState} from 'react';
+import { View } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 import StyleSheet, { estilos } from '../styles/styles';
 import ButtonAndroidComponent from '../components/ButtonAndroidComponent';
 import ButtonIOSComponent from '../components/ButtonIOSComponent';
+import InputTextComponent from '../components/InputTextComponent';
 
 export function navigationOptions({ navigate }) {
   return {
@@ -17,26 +18,69 @@ const ButtonComponent = Platform.select({
   android: () => ButtonAndroidComponent,
 })();
 
-const Doar = props => (
-  <LinearGradient
-    colors={ ['#3CB371', '#2E8B57', '#008000', '#228B22'] }
-    style={ estilos.container }
-  >
-    <View style={ [estilos.container, { justifyContent: 'space-around', alignItems: 'center' }] }>
-      <View style={ estilo.container }>
-        <TextInput placeholder="Produto" style={ [estilo.input] } />
-        <View style={ { width: '100%', borderRadius: 20 } }>
-          <TextInput placeholder="Descrição" style={ [estilo.input, { marginTop: 15, height: 150, textAlignVertical: 'top' }] } />
+const registrar = async entidadeObject => {
+
+  setVisible(true);
+  const entidade = await CadastrarEntidade(entidadeObject).then((result) => {
+    setVisible(false);
+    navigate('confirmaCadastro')
+  }).catch((error) => {
+    setVisible(false);
+    AlertComponent('Erro ao Cadastrar', error)
+  })
+  console.log(entidade)
+};
+
+const Doar = props => {
+
+  const [produto, setProduto] = useState('');
+  const [descricao, setDescricao] = useState('');
+
+  return (
+    <LinearGradient colors={ ['#579054', '#304250'] } style={ estilos.container }>
+      <View
+        style={ [
+          estilos.container,
+          { justifyContent: 'space-around', alignItems: 'center' }
+        ] }
+      >
+        <View style={ estilo.container }>
+          <InputTextComponent placeholder="Produto" setValue={ setProduto } value={produto} style={ [estilo.input] } />
+          <View style={ { width: '100%', borderRadius: 20 } }>
+            <InputTextComponent
+              placeholder='Descrição'
+              style={ [
+                estilo.input,
+                { marginTop: 15, height: 150, textAlignVertical: 'top' }
+              ] }
+              setValue={ setDescricao }
+              value={descricao}
+            />
+          </View>
         </View>
-      </View>
-      <View>
         <View>
-          <ButtonComponent title="Cadastrar Doação" />
+          <View>
+            <ButtonComponent
+              onPressHandler={ async () => {
+                await registrar({
+                  active : true,
+                  products : products: {
+                    produto: produto,
+                    descricao: descricao,
+                  },
+                  entidade : 'entidade',
+                  mercadoria : mercadoria,
+                  updatedAt : updatedAt,
+                });
+              } }
+              title='Cadastrar Doação'
+            />
+          </View>
         </View>
       </View>
-    </View>
-  </LinearGradient>
-);
+    </LinearGradient>
+  );
+};
 
 const DoarScreen = {
   screen: Doar,
